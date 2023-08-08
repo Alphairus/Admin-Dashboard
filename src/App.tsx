fe3fc0410abb28cc272c950ac6d1744cd0f99fa1
Alphairus <cmcauliffe006@gmail.com>
2023-08-08 14:27:49 -0400

    React-router setup for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import Home from "../pages/home/Home.tsx";
+import Users from "../pages/users/Users.tsx";
+import Products from "../pages/products/Products.tsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Home/>
+      ),
+    },
+    {
+      path: "users",
+      element: <Users/>,
+    },
+    {
+      path: "products",
+      element: <Products/>,
+    },
+  ]);
+
   return (
-    <div><Home/></div>
+    <RouterProvider router={router} />
   )
 }
 
-export default App
+export default App;
