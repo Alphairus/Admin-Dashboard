5ea5fca97da06b4224bb5c615dc39c41ed6142b6
Alphairus <cmcauliffe006@gmail.com>
2023-08-08 14:48:07 -0400

    Adding more pages and layouts/components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,60 @@
-import Home from "../pages/home/Home.tsx";
-import Users from "../pages/users/Users.tsx";
-import Products from "../pages/products/Products.tsx";
+import Home from "./pages/home/Home.tsx";
+import Users from "./pages/users/Users.tsx";
+import Products from "./pages/products/Products.tsx";
+import Login from "./pages/login/Login.tsx";
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
+  Outlet,
 } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar.tsx";
+import Footer from "./components/footer/Footer.tsx";
+import Menu from "./components/menu/Menu.tsx";
 
 
 function App() {
 
+
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: (
-        <Home/>
-      ),
+      element:<Layout/>,
+      children: [
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path:"/users",
+          element:<Users/>
+        },
+        {
+          path:"/products",
+          element:<Products/>
+        },
+      ]
     },
     {
-      path: "users",
-      element: <Users/>,
-    },
-    {
-      path: "products",
-      element: <Products/>,
-    },
+      path:"/login",
+      element: <Login />,
+    }
   ]);
 
   return (
