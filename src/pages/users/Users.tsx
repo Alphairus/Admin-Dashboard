caff8733eeefc1a40ec1b3e3b199fedc3e4400e4
Alphairus <cmcauliffe006@gmail.com>
2023-08-14 15:45:12 -0400

    User table using MUI DataGrid

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,10 +1,88 @@
-import React from 'react'
 import "./users.scss"
+import DataTable from "../../components/dataTable/DataTable";
+import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { userRows } from "../../data";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 150,
+  },
+  {
+    field: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 150,
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
+  // {
+  //     field:"actions", 
+  //     headerName:"Actions", 
+  //     width:100,
+  //     renderCell: (params)=>{
+  //         return <div className="action">
+  //         <div className="view">View</div>
+  //         <div className="delete">Delete</div>
+  //         </div>
+  //     }
+  // },
+  // {
+  // field:"status", headerName:"Status", width:100, type:"boolean"
+  // },
+  // {
+  //   field: 'fullName',
+  //   headerName: 'Full name',
+  //   description: 'This column has a value getter and is not sortable.',
+  //   sortable: false,
+  //   width: 160,
+  //   valueGetter: (params: GridValueGetterParams) =>
+  //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  // },
+
 
 const Users = () => {
   return (
     <div className='users'>
-      Users
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows}/>
     </div>
   )
 }
